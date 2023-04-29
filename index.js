bd413cdfb591699fd36b07f41e7ddf19b82406c2
ThangPham <phamducthang2503200@gmail.com>
2023-04-29 19:42:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//include the libary
+
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//engine
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views","./views");
+
+//use libary
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
+app.use(session({
+    name:process.env.SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret:"abcdef",
+    cookie: {
+        httpOnly:true,
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite: true,
+        secure:process.env.MODE_ENV === 'production'
+    }
+}));
+
+
+
+app.get("/",function(req,res){
+    res.render("login");
+});
+
+
+//set port
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`sever start on port ${PORT}`);
+});
